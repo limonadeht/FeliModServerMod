49cd6b258c02d71b6bde5f7c055af6267f12c270
isuzu-shiranui <takashi9714@gmail.com>
2015-10-03 13:50:37 +0900

    ツールチップ表示

--- src/common/food/itemYakinikudonburi_mk4.java
+++ src/common/food/itemYakinikudonburi_mk4.java
@@ -49,7 +49,6 @@ public class itemYakinikudonburi_mk4 extends Item
 	//ToolTipの設定。EnumChatFormattingでカラーコードが指定可能
     public void addInformation(ItemStack itemStack, EntityPlayer player, List list, boolean advanced) {
 		if (FeliModServerMod.serverproxy.isShiftKeyDown()) {
-			list.add("焼肉丼");
 	        list.add(EnumChatFormatting.DARK_GREEN + "Compressed: 32");
 	        list.add(EnumChatFormatting.GOLD + "Hunger: " + healAmount);
 	        list.add(EnumChatFormatting.DARK_AQUA + "mogumogu: " + itemUseDuration);
@@ -64,6 +63,7 @@ public class itemYakinikudonburi_mk4 extends Item
 	        }
 	        list.add(EnumChatFormatting.DARK_GRAY + "Durability: " + itemStack.getItemDamage() + "/" + this.getMaxDamage());
 		}else{
+			list.add("焼肉丼");
 			list.add(EnumChatFormatting.ITALIC + "LShift: Expand tooltip.");
 		}
         
